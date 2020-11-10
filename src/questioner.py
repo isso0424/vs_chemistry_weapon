d1777fa6fc3dce1f1104dc59141bc4cd52735d5b
isso0424 <isso04240129@gmail.com>
2020-11-10 11:01:26 +0900

    docs: add docs for src/questioner

--- src/questioner.py
+++ src/questioner.py
@@ -6,12 +6,22 @@ from src.types.question import Question
 
 
 class Questioner:
+    """
+    Question proposing interprinter
+    """
     def __init__(self, worksheet):
         self.worksheet = worksheet
         self.collect_count = 0
         self.wrong_count = 0
 
     def load_question(self, index: int) -> Question:
+        """
+        Create question object from index
+        params
+        ------
+        index: int
+            question row number
+        """
         question_text = get_cells(self.worksheet, f"I{index}").value
         answer = get_cells(self.worksheet, f"J{index}").value
         wrong_cells = get_cells(self.worksheet, f"K{index}:M{index}")[0]
@@ -21,6 +31,13 @@ class Questioner:
         return question
 
     def proposing(self, question: Question):
+        """
+        Proposing a question
+        params
+        ------
+        question: Question
+            question is proposed.
+        """
         text = question.create_question()
         print(text)
         answer = int(input(">>>"))
@@ -34,6 +51,9 @@ class Questioner:
 
 
     def start(self):
+        """
+        start interprinter
+        """
         for index in range(4, 45):
             question = self.load_question(index)
             self.proposing(question)
